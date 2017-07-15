375a2ac8ef90163ebb33aaf8de2b269013847214
Ben Whitman <ben.whitman@outlook.com>
2017-07-15 19:59:42 +0100

    fixed a couple of issues with WhosOutInDepartment

--- timetastic.js
+++ timetastic.js
@@ -129,7 +129,7 @@ exports.checkHolidaysForDepartment = function (departmentId, slots, callback, cl
         if (response.statusCode === 200) {
             callback(null, close(outputSessionAttributes, 'Fulfilled', {
                 contentType: 'PlainText',
-                content: Formatters.holidayList(JSON.parse(body).holidays, false, true, 'Here are the holidays for ' + slots.department, 'There were no matching bookings')
+                content: Formatters.holidayList(JSON.parse(body).holidays, false, true, 'Here are the holidays for ' + slots.department + ' on ' + slots.date, 'There were no matching bookings')
             }));
         } else {
             callback(null, close(outputSessionAttributes, 'Fulfilled', {
